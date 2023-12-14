e1eb09466e573020ee3adc42c8100fe4456fd766
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-14 23:01:42 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.textContent === "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.textContent === "Del") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.textContent;
+      input.value = string;
+    }
+  });
+});
